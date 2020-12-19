647e1d41771b2a76c90d8eb087208460a3ecd2e4
Rodrigo Chagas de Miranda <rcmiranda2@stefanini.com>
2020-12-19 01:23:00 -0300

    Desafio 5 da trilha de desenvolvedor Java

--- src/main/java/br/com/chagas/desafio/fundamentos/aritimeticos/ConsumoMedioAutomovel.java
+++ src/main/java/br/com/chagas/desafio/fundamentos/aritimeticos/ConsumoMedioAutomovel.java
@@ -1,5 +1,30 @@
 package br.com.chagas.desafio.fundamentos.aritimeticos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.util.Locale;
+import java.util.StringTokenizer;
+
 public class ConsumoMedioAutomovel {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        Locale.setDefault(new Locale("en", "US"));
+
+        int distancia = 0;
+        double consumo = 0.0;
+
+        distancia = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(br.readLine());
+        consumo = Double.parseDouble(st.nextToken());
+
+
+        System.out.printf("%.3f km/l", distancia / consumo);
+
+
+        
+    }
 }
